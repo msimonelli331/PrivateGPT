3ba77084d24b969e8d16d1c74892ece4b3febfca
Matt Simonelli <matt.simonelli8@gmail.com>
2025-12-29 14:03:03 -0500

    Fix ingestion

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = 'run' ]; then
     find /files/input/ -name *".ico" -exec rm {} \;
-    [ -f /files/input ] && PGPT_PROFILES=ingest make ingest /files/input
+    [ -d /files/input ] && PGPT_PROFILES=ollama make ingest /files/input
     PGPT_PROFILES=ollama make run
 else
     exec "$@"
